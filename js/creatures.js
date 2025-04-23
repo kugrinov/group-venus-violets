85a7dc7e9d15f9f67251b538f871ccb2292e2f2e
Kalina U <kugrinov@iu.edu>
2025-04-23 13:43:13 -0400

    created custom creature form with databse

--- js/creatures.js
+++ js/creatures.js
@@ -1,12 +1,59 @@
 import { app } from "./app.js";
 
-import { getFirestore, getDocs, collection, updateDoc, deleteDoc, doc, } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { getFirestore, getDocs, collection,addDoc, updateDoc, deleteDoc, doc, } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 const db = getFirestore(app);
 
+const creatureCollection = collection(db,"newcreatures");
+
+const newCreatureFormRef = document.querySelector("#custom-creature");
+const creaturesRef = document.querySelector("#customCreatures");
+
+async function addNewCreature(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    await addDoc(creatureCollection, {creatureName: formData.get("creatureName"),
+        creatureHeads: formData.get("creatureHeads"),
+        creatureColors: formData.get("creatureColors"),
+        creatureAbility: formData.get("creatureAbility"),
+    });
+
+    getNewCreatures();
+    
+}
+
+async function getNewCreatures() {
+    creaturesRef.innerHTML ="";
+
+    const creatureList = await getDocs(creatureCollection);
+
+    for (let index = 0; index < creatureList.docs.length; index++) {
+        const currentCreature = creatureList.docs[index];
+        const creatureData = currentCreature.data();
+
+        creaturesRef.innerHTML +=`
+        <div>
+        <h4>${creatureData.creatureName}</h4>
+        <ul>
+        <li>Number of Heads: ${creatureData.creatureHeads}</li>
+        <li>Colors: ${creatureData.creatureColors}</li>
+        <li>Ability: ${creatureData.creatureAbility}</li>
+        </ul>
+        </div>
+        `
+        
+    }
+}
+
+newCreatureFormRef.onsubmit = addNewCreature;
+
+getNewCreatures();
 
 
-// const creatureCollection = collection(db, "creatures")
+
+
 
 // const creaturesRef = document.querySelector("#customCreatures")
 // const creatureFormRef = document.querySelector("#custom-creature")
